d297de9a9985e462d285b0aabd794c46c371188b
Kouhei <kouhei.ito@itolab-ktc.com>
2022-08-29 12:42:27 +0900

    Add files via upload

--- pwm.cpp
+++ pwm.cpp
@@ -9,15 +9,19 @@ uint8_t ESC_calib=0;
 //ファイル内グローバル変数
 const uint Slice_num_rear=1;
 const uint Slice_num_front=2;
+const uint Slice_num_servo = 3;
 
 void pwm_init()
 {
     // PWMの設定
-    // Tell GPIO 2-5 they are allocated to the PWM for Motor Control
+    // Tell GPIO 2-6 they are allocated to the PWM for Motor & Servo Control
     gpio_set_function(2, GPIO_FUNC_PWM);//Rear  Left  (RL) Motor PWM
     gpio_set_function(3, GPIO_FUNC_PWM);//Rear  Right (RR) Motor PWM
     gpio_set_function(4, GPIO_FUNC_PWM);//Front Left  (FL) Motor PWM
     gpio_set_function(5, GPIO_FUNC_PWM);//Front Right (FR) Motro PWM
+    gpio_set_function(6, GPIO_FUNC_PWM);//Servo PWM
+
+
 
     // Find out which PWM slice is connected to GPIO 0 (it's slice 0)
     // Set period T
@@ -25,8 +29,12 @@ void pwm_init()
     // T=(24999+1)*100/125e6=25000e2/125e6=200e-4=0.02s(=50Hz)
     pwm_set_wrap(Slice_num_front, 3124);
     pwm_set_wrap(Slice_num_rear,  3124);
+    pwm_set_wrap(Slice_num_servo, 3124);
+
     pwm_set_clkdiv(Slice_num_front, 100.0);
     pwm_set_clkdiv(Slice_num_rear, 100.0);
+    pwm_set_clkdiv(Slice_num_servo, 100.0);
+
     pwm_clear_irq(Slice_num_front);
     pwm_set_irq_enabled(Slice_num_front, true);
     irq_set_exclusive_handler(PWM_IRQ_WRAP, MAINLOOP);
@@ -53,14 +61,18 @@ void pwm_init()
     // Set the PWM running
     //sleep_ms(500);
     pwm_set_enabled(Slice_num_front, true);
-    pwm_set_enabled(Slice_num_rear, true);
-    
+    pwm_set_enabled(Slice_num_rear,  true);
+    pwm_set_enabled(Slice_num_servo, true);
+
     sleep_ms(3000);
     
     pwm_set_chan_level(Slice_num_front, PWM_CHAN_A, DUTYMIN);
     pwm_set_chan_level(Slice_num_front, PWM_CHAN_B, DUTYMIN);
     pwm_set_chan_level(Slice_num_rear,  PWM_CHAN_A, DUTYMIN);
     pwm_set_chan_level(Slice_num_rear,  PWM_CHAN_B, DUTYMIN);
+    pwm_set_chan_level(Slice_num_rear,  PWM_CHAN_B, DUTYMIN);
+    pwm_set_chan_level(Slice_num_servo, PWM_CHAN_A, 625);
+
     sleep_ms(1000);
 }
 
